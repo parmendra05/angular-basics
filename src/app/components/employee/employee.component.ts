e3a7b56ce64b6aa36c69035dfef24d6d4b95a76b
parmendra05 <parmendrakumar718@gmail.com>
2025-05-20 21:56:10 +0530

    display list of data on html page

--- src/app/components/employee/employee.component.ts
+++ src/app/components/employee/employee.component.ts
@@ -1,14 +1,19 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-employee',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './employee.component.html',
   styleUrl: './employee.component.css'
 })
 export class EmployeeComponent {
 
-  name ="Ramesh"
-  address ="Bangalore"
+  employees = [
+    {name: "Ramesh" , address : "Bangalore" , salary : 70000 },
+    {name: "Ganesh" , address : "Hyderabad" , salary : 55000 },
+    {name: "Mahesh" , address : "Delhi" , salary : 45000 },
+    {name: "Suresh" , address : "Bangalore" , salary : 40000 }
+  ]
 
 }
