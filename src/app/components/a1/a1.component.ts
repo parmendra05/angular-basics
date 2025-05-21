6c73bef0190e4f9b91019e73b1808f79ecd8857f
parmendra05 <parmendrakumar718@gmail.com>
2025-05-21 11:17:42 +0530

    created component a1,a2,b & created service counterService

---
+++ src/app/components/a1/a1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-a1',
+  imports: [],
+  templateUrl: './a1.component.html',
+  styleUrl: './a1.component.css'
+})
+export class A1Component {
+
+}
