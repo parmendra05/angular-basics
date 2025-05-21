d9a63ab6346b79b9a3dfabf7910bc4d76e3e56dc
parmendra05 <parmendrakumar718@gmail.com>
2025-05-21 08:02:41 +0530

    HostListener

--- src/app/directives/my-line-hilighter.directive.ts
+++ src/app/directives/my-line-hilighter.directive.ts
@@ -1,13 +1,25 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appMyLineHilighter]'
 })
 export class MyLineHilighterDirective {
 
-  constructor() { }
+  constructor( private element : ElementRef) { }
 
   @HostBinding("style.backgroundColor")  // HostBindinng will change the properties of Host (HTML Tags)
   bgcolor = "green" // attribute name will be anything
+
+  @HostListener("mouseover") // HostListener used to perform some action with Host (HTML)
+  changeFontSize(){
+    this.element.nativeElement.style.fontSize ="50px"
+    console.log("mouse enter")
+  }
+
+  @HostListener("mouseout")
+  resetFontSize(){
+    this.element.nativeElement.style.fontSize ="30px"
+    console.log("mouse Out")
+  }
 
 }
