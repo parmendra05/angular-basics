65c7641e6277669b60c609f8e28cb17557770481
parmendra05 <parmendrakumar718@gmail.com>
2025-05-21 12:02:46 +0530

    independent state management

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { RouterOutlet } from '@angular/router';
 import { EmployeeComponent } from './components/employee/employee.component';
 import { A1Component } from './components/a1/a1.component';
 import { A2Component } from './components/a2/a2.component';
+import { BComponent } from './components/b/b.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, EmployeeComponent,A1Component,A2Component],
+  imports: [RouterOutlet, EmployeeComponent,A1Component,A2Component,BComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/components/b/b.component.ts
+++ src/app/components/b/b.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { CounterService } from '../../services/counter.service';
 
 @Component({
   selector: 'app-b',
   imports: [],
+  providers:[CounterService],
   templateUrl: './b.component.html',
   styleUrl: './b.component.css'
 })
 export class BComponent {
-
+constructor(public counterService: CounterService){}
 }
